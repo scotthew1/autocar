e0b87ce4b69ced5996bada4ac3b48ffb27fd9309
zmzegare <zmzegare@gmail.com>
2014-02-25 23:14:25 -0500

    Added functions for starting/stopping/adjustiing

--- arduino/autocar_slave/autocar_slave.ino
+++ arduino/autocar_slave/autocar_slave.ino
@@ -2,11 +2,10 @@
 #include <Wire.h>
 
 long M1_cur, M2_cur, M1_val, M2_val;
+int main_power;
 
 void setup(){
   Serial.begin(9600);
-  Mx.SetMode(Mx_M1, SLEW);
-  Mx.SetMode(Mx_M2, SLEW);
 }
 
 ISR(TIMER1_COMPA_vect){
@@ -14,15 +13,19 @@ ISR(TIMER1_COMPA_vect){
 }
 
 void loop(){
+  start_motors(50);
+  delay(5000);
+  stop_motors();
+  delay(2000);
   CCW_90();
   delay(5000);
-  CW_90();
-  delay(5000);
-  CCW_180();
-  delay(5000);
+  stop_motors();
+  delay(2000);
 }
 
 void CCW_90(){  
+  Mx.SetMode(Mx_M1, SLEW);
+  Mx.SetMode(Mx_M2, SLEW);
   M1_cur = Mx.MotorPosition(Mx_M1);
   delay(500);
   M2_cur = Mx.MotorPosition(Mx_M2);
@@ -30,9 +33,11 @@ void CCW_90(){
   M2_val = M2_cur + 415;
   Mx.MotorTarget(Mx_M1, M1_val, 15);
   Mx.MotorTarget(Mx_M2, M2_val, 15);
-  delay(2000);
+  delay(1000);
 }
 void CW_90(){  
+  Mx.SetMode(Mx_M1, SLEW);
+  Mx.SetMode(Mx_M2, SLEW);
   M1_cur = Mx.MotorPosition(Mx_M1);
   delay(500);
   M2_cur = Mx.MotorPosition(Mx_M2);
@@ -40,9 +45,11 @@ void CW_90(){
   M2_val = M2_cur - 415;
   Mx.MotorTarget(Mx_M1, M1_val, 15);
   Mx.MotorTarget(Mx_M2, M2_val, 15);
-  delay(2000);
+  delay(1000);
 }
 void CCW_180(){  
+  Mx.SetMode(Mx_M1, SLEW);
+  Mx.SetMode(Mx_M2, SLEW);
   M1_cur = Mx.MotorPosition(Mx_M1);
   delay(500);
   M2_cur = Mx.MotorPosition(Mx_M2);
@@ -50,5 +57,30 @@ void CCW_180(){
   M2_val = M2_cur + 825;
   Mx.MotorTarget(Mx_M1, M1_val, 15);
   Mx.MotorTarget(Mx_M2, M2_val, 15);
-  delay(2000);
+  delay(1000);
+}
+void start_motors(int power){
+  Mx.SetMode(Mx_M1, FLOAT);
+  Mx.SetMode(Mx_M2, FLOAT+INV);
+  main_power = power;
+  Mx.SetMotors(Mx_M1+Mx_M2, 50);
+  delay(1000);
+}
+void stop_motors(){
+  Mx.SetMode(Mx_M1, FLOAT);
+  Mx.SetMode(Mx_M2, FLOAT_INV);
+  Mx.SetMotors(Mx_M1+Mx_M2, 0);
+  delay(1000);
+}
+void inc_power(char motor){
+  Mx.SetMode(Mx_M1, FLOAT);
+  Mx.SetMode(Mx_M2, FLOAT+INV);
+  Mx.SetMotors(motor, main_power+5);
+  delay(1000);
+}
+void dec_power(char motor){
+  Mx.SetMode(Mx_M1, FLOAT);
+  Mx.SetMode(Mx_M2, FLOAT+INV);
+  Mx.SetMotors(motor, main_power-5);
+  delay(1000);
 }
