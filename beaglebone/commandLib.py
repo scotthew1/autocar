eb65a0ebf57c299020f14c831c6e3e569c27687b
Scott Reisor <scotthew1@mac.com>
2014-03-20 15:42:46 -0400

    stricter function inputs, all commands output 4 bytes, cleaned up code/comments

--- beaglebone/commandLib.py
+++ beaglebone/commandLib.py
@@ -4,55 +4,89 @@
 
 from bbio import *  #for when the beaglebone is hooked up
 
-#Set Serial1 to a 9600 baud rate
-def setup():
 
+def setup():
+	"""
+	Set up the UART communication to baud rate 9600
+	"""
  	Serial1.begin(9600)
 
-def Stop ():
-
+def Stop():
+	"""
+	Stop the car
+	"""
 	command = 'a' + "000"
 	Serial1.write(command)
 
-def Start (speed): # Starts the car, if speed is too high or negative exception is raised.
-
-	if speed < 0 or speed > 100:
+def Start( speed ): 
+	"""
+	Starts the car
+	if speed is too high or negative, a ValueError exception is raised.
+	"""
+	if speed < 1 or speed > 99:
 		raise ValueError
-	command = 'b' + str(speed)
+	if speed < 10:
+		command = 'b' + '0' + str(speed) + '0'
+	else:
+		command = 'b' + str(speed) + '0'
 	Serial1.write(command)
 
-def leftTurn ():
-
+def leftTurn():
+	"""
+	Turn the car left 90 degrees
+	"""
 	command = 'c' + "000"
 	Serial1.write(command)
 
-def rightTurn ():
-
+def rightTurn():
+	"""
+	Turn the car right 90 degrees
+	"""
 	command = 'd' + "000"
 	Serial1.write(command)
 
-def turnAround ():
-
+def turnAround():
+	"""
+	Turn the car around 180 degrees
+	"""
 	command = 'e' + "000"
 	Serial1.write(command)
 
-def increasePower (motor, speed):   # Increases power to a motor of choice, if the speed is too high or low
-									# an exception is raised.
+def increasePower( motor, speed ):   
+	"""
+	Increases power to a motor of choice
+	if the motor input is malformed, a ValueError exception is raised.
+	if the speed is too high or low, a ValueError exception is raised.
+	"""
+	if !isinstance(motor, basestring) or len(motor) != 2:
+		raise ValueError
 	if speed < 0 or speed > 9:
 		raise ValueError
-	command = 'f' + motor.decode("hex") + str(speed)
+	command = 'f' + motor.decode("hex") + str(speed) + '0'
 	Serial1.write(command)
 
-def decreasePower (motor, speed):  # Decreases power to a motor of choice, if the speed is too high or low
-								   # an exception is raised.
+def decreasePower( motor, speed ):  
+	"""
+	Decreases power to a motor of choice
+	if the motor input is malformed, a ValueError exception is raised.
+	if the speed is too high or low, a ValueError exception is raised.
+	"""
+	if !isinstance(motor, basestring) or len(motor) != 2:
+		raise ValueError
 	if speed < 0 or speed > 9:
 		raise ValueError
-	command = 'g' + motor.decode("hex") + str(speed)
+	command = 'g' + motor.decode("hex") + str(speed) + '0'
 	Serial1.write(command)
 
-def Reverse (speed):  # Reverses the car, if the speed is too high or negative, an exception is raised.
-
-	if speed < 0 or speed > 100:
+def Reverse( speed ):  
+	"""
+	Reverses the car
+	if speed is too high or negative, a ValueError exception is raised.
+	"""
+	if speed < 1 or speed > 99:
 		raise ValueError
-	command = 'h' + str(speed)
+	if speed < 10:
+		command = 'h' + '0' + str(speed) + '0'
+	else:
+		command = 'h' + str(speed) + '0'
 	Serial1.write(command)
