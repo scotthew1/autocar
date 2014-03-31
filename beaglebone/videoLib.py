013d02c2ae9c6e3ebe0d8fb5a0068c8f495028de
scott reisor <scotthew1@mac.com>
2014-03-31 03:03:51 -0400

    road line detection now looks for intersection point of exteded edge lines

--- beaglebone/videoLib.py
+++ beaglebone/videoLib.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import cv2
 import cv
 import numpy as np
-import time
-import argparse
 
 class VideoCapture:
 	frameCount = 0
@@ -80,15 +79,73 @@ class VideoCapture:
 		else:
 			raise Exception( "No frame to preview." )
 
+	def getAvgLine( self, lines ):
+		outLine = [0, 0, 0, 0]
+		length  = len( lines )
+		for l in lines:
+			outLine[0] += l[0]
+			outLine[1] += l[1]
+			outLine[2] += l[2]
+			outLine[3] += l[3]
+		outLine[0] = outLine[0] / length
+		outLine[1] = outLine[1] / length
+		outLine[2] = outLine[2] / length
+		outLine[3] = outLine[3] / length
+		return outLine
+
+	def getLineIntersectX( self, l1, l2 ):
+		"""
+		With help from http://en.wikipedia.org/wiki/Line–line_intersection
+		x1 = l1[0]; y1 = l1[1]; x2 = l1[2]; y2 = l1[3]
+		x3 = l2[0]; y3 = l2[1]; x4 = l2[2]; y4 = l2[3]
+
+		(x1*y2 - y1*x2)(x3-x4) - (x1-x2)(x3*y4 - y3*x4)
+		-----------------------------------------------
+		         (x1-x2)(y3-y4)-(y1-y2)(x3-x4)
+		"""
+		return ( (l1[0]*l1[3] - l1[1]*l1[2])*(l2[0]-l2[2]) - (l1[0]-l1[2])*(l2[0]*l2[3] - l2[1]*l2[2]) ) / ( (l1[0]-l1[2])*(l2[1]-l2[3])-(l1[1]-l1[3])*(l2[0]-l2[2]) )
+
 	def findLines( self ):
 		if self.currentFrame is None:
 			raise Exception( "No frame to process." )
-		edges = cv2.Canny( self.currentFrame, 50, 200 )
-		# gray  = cv2.cvtColor( edges, cv.CV_RGB2GRAY )
-		lines = cv2.HoughLinesP( edges, 1, cv.CV_PI/180, 50 )
+		gray  = cv2.cvtColor( self.currentFrame, cv.CV_RGB2GRAY )
+		ret, thresh = cv2.threshold( gray, 230, 255, cv2.THRESH_BINARY )
+		edges = cv2.Canny( thresh, 50, 100 )
+		lines = cv2.HoughLinesP( edges, 1, np.pi/180, 60, maxLineGap=10 )
 		lineFrame = self.currentFrame
-		for l in lines[0]:
-			cv2.line( lineFrame, (l[0],l[1]), (l[2],l[3]), (0,0,255), 3 )
+		if lines is not None:
+			# arrays of grouped lines
+			( left, right ) = [], []
+			# loop for HoughLinesP lines
+			midpoint = (self.width/2)
+			for l in lines[0]:
+				# x1 = l[0]; y1 = l[1]; x2 = l[2]; y2 = l[3]
+				yRatio = l[1] / float(l[3])
+				if .9 <= yRatio <= 1.1:
+					# mostly horizontal
+					cv2.line( lineFrame, (l[0],l[1]), (l[2],l[3]), (0,0,255), 3 )
+				elif l[0] < midpoint and l[2] < midpoint:
+					# left side of the screen
+					left.append( l )
+				elif l[0] > midpoint and l[2] > midpoint:
+					# right side of the screen
+					right.append( l )
+				else:
+					# don't know
+					pass
+			l1 = None
+			l2 = None
+			if len( left ) > 0:
+				l1 = self.getAvgLine( left )
+				cv2.line( lineFrame, (l1[0],l1[1]), (l1[2],l1[3]), (0,255,0), 3 )
+			if len( right ) > 0:
+				l2 = self.getAvgLine( right )
+				cv2.line( lineFrame, (l2[0],l2[1]), (l2[2],l2[3]), (255,0,0), 3 )
+			if l1 is not None and l2 is not None:
+				xIntersect = self.getLineIntersectX( l1, l2 )
+				cv2.circle( lineFrame, (xIntersect, self.height/2), 4, (0,255,255) )
+		else:
+			print "no lines detected"
 		return lineFrame
 
 	def findShapes( self ):
@@ -124,29 +181,34 @@ class VideoCapture:
 
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser()
+	from time import time, sleep
+	from argparse import ArgumentParser
+
+	parser = ArgumentParser()
 	parser.add_argument( "-i", "--infile", help="use video from an input file rather than the camera" )
 	parser.add_argument( "-o", "--outfile", help="output processed to a file" )
 	parser.add_argument( "--fourcc", help="four character code to specify video output type", default="XVID" )
 	parser.add_argument( "-s", "--show", help="show the output on screen", action="store_true" )
 	parser.add_argument( "-n", "--noprocess", help="disable image processing", action="store_true" )
-	parser.add_argument( "-l", "--framelimit", type=int, help="number of frames to capture", default=100 )
+	parser.add_argument( "-l", "--framelimit", type=int, help="number of frames to capture" )
 	args = parser.parse_args()
 
 	print args
 
 	vc = VideoCapture( infile=args.infile, outfile=args.outfile, fourcc=args.fourcc, preview=args.show )
 
-	t0 = time.time()
-	while vc.captureFrame() and vc.frameCount <= args.framelimit:
-		print "finding lines..."
+	t0 = time()
+	while vc.captureFrame():
 		lineFrame = vc.findLines()
 		if args.outfile:
 			vc.writeFrame( lineFrame )
 		if args.show:
 			vc.previewFrame( lineFrame )
+			sleep( 0.03 )
+		if args.framelimit and vc.frameCount <= args.framelimit:
+			break
 
-	t1 = time.time()
+	t1 = time()
 	tt = t1 - t0
 	
 	print "frames: %d" % vc.frameCount
