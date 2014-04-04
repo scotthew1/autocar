169c8ec8a71715135b86e7e21d87b524af914c07
zmzegare <zmzegare@gmail.com>
2014-04-04 15:16:12 -0400

    Update to LED library

--- beaglebone/LED.py
+++ beaglebone/LED.py
@@ -1,51 +1,22 @@
 from bbio import *
 import Adafruit_BBIO.GPIO as GPIO
 
+OFF = (0,0,0)
+RED = (1,0,0)
+GREEN = (0,1,0)
+BLUE = (0,0,1)
+YELLOW = (1,1,0)
+PURPLE = (1,0,1)
+
 def setup():
 	GPIO.setup("P8_7", GPIO.OUT)	# RED
 	GPIO.setup("P8_8", GPIO.OUT)	# GREEN
 	GPIO.setup("P8_9", GPIO.OUT)	# BLUE
 
-def LED():
-	print("CLEAR")
-	GPIO.output("P8_7", GPIO.LOW)
-	GPIO.output("P8_8", GPIO.LOW)
-	GPIO.output("P8_9", GPIO.LOW)
-	delay(3000)
-	print("RED")
-	GPIO.output("P8_7", GPIO.HIGH)
-	GPIO.output("P8_8", GPIO.LOW)
-	GPIO.output("P8_9", GPIO.LOW)
-	delay(3000)
-	print("GREEN")
-	GPIO.output("P8_7", GPIO.LOW)
-	GPIO.output("P8_8", GPIO.HIGH)
-	GPIO.output("P8_9", GPIO.LOW)
-	delay(3000)
-	print("BLUE")
-	GPIO.output("P8_7", GPIO.LOW)
-	GPIO.output("P8_8", GPIO.LOW)
-	GPIO.output("P8_9", GPIO.HIGH)
-	delay(3000)
-	print("GREEN + RED")
-	GPIO.output("P8_7", GPIO.HIGH)
-	GPIO.output("P8_8", GPIO.HIGH)
-	GPIO.output("P8_9", GPIO.LOW)
-	delay(3000)
-	print("GREEN + BLUE")
-	GPIO.output("P8_7", GPIO.LOW)
-	GPIO.output("P8_8", GPIO.HIGH)
-	GPIO.output("P8_9", GPIO.HIGH)
-	delay(3000)
-	print("RED + BLUE")
-	GPIO.output("P8_7", GPIO.HIGH)
-	GPIO.output("P8_8", GPIO.LOW)
-	GPIO.output("P8_9", GPIO.HIGH)
-	delay(3000)
-	print("RED + GREEN + BLUE")
-	GPIO.output("P8_7", GPIO.HIGH)
-	GPIO.output("P8_8", GPIO.HIGH)
-	GPIO.output("P8_9", GPIO.HIGH)
+def LED(color = OFF):
+	GPIO.output("P8_7", color[0])
+	GPIO.output("P8_8", color[1])
+	GPIO.output("P8_9", color[2])
 	delay(3000)
 
 run(setup,LED)
