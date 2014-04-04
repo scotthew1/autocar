18974098bc8d75de41ca4264548c144bedff06f3
zmzegare <zmzegare@gmail.com>
2014-04-04 17:49:33 -0400

    Took unneeded line out of LoggingTest.py

--- beaglebone/LoggingTest.py
+++ beaglebone/LoggingTest.py
@@ -8,7 +8,6 @@ st = datetime.datetime.fromtimestamp(ts).strftime('%m-%d-%Y_%H-%M-%S')
 path = 'log/%s.log' % st
 log_path = '../log/%s.log' % st
 
-os.path.exists(path)
 logging.basicConfig(filename=log_path, level=logging.DEBUG)
 logging.info(st)
 logging.warning('MONGOLIAN')
