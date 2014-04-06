702eb700a146e488d23aa7b3ebff61cf73e8d7bb
beaglebone <scotthew1@mac.com>
2014-04-05 21:41:33 -0400

    beaglebone can now nudge in different levels

--- beaglebone/movementTest.py
+++ beaglebone/movementTest.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python
 
 from bbio import *
+from cv2 import circle
 from videoLib import VideoCapture
 import commandLib as cl
 
+
+nudgeHold = 10
+
 def mainLoop():
-	vc = VideoCapture( outfile="sample_video/nudge_movement0.avi" )
+	vc = VideoCapture( outfile="sample_video/nudge_movement_imp.avi" )
 	lastNudge = 0
 
 	intersect = None
 	print "startup loop"
-	while intersect is None and vc.captureFrame():
+	while vc.frameBuf.size() < 5 and vc.captureFrame():
 		frame, intersect = vc.findLines()
+		vc.drawGrid( frame )
+		vc.writeFrame( frame )
+		if intersect:
+			vc.saveFrameToBuf()
 
 	print "sending start"
 	cl.flush()
@@ -25,19 +33,42 @@ def mainLoop():
 	# t0 = time.time()
 	while vc.captureFrame() and vc.frameCount < 100:
 		frame, intersect = vc.findLines()
-		if intersect and intersect > (vc.width/2 + 20) and vc.frameCount >= (lastNudge+10):
-			print "NUDGE M2!!"
+		nudgeMotor = None
+		nudgeTime  = None
+		if intersect and vc.frameCount >= (lastNudge+15):
+			if intersect > (vc.width/2 + 60):
+				print "BIGGER NUDGE M2!!"
+				nudgeMotor = cl.M2
+				nudgeTime  = 7
+				circle( frame, (20, vc.height-20), 6, (0,0,255), 2 )
+			elif intersect > (vc.width/2 + 30):
+				print "BIG NUDGE M2!!"
+				nudgeMotor = cl.M2
+				nudgeTime  = 4
+				circle( frame, (20, vc.height-20), 4, (0,0,255), 2 )
+			elif intersect > (vc.width/2 + 15):
+				print "NUDGE M2!!"
+				nudgeMotor = cl.M2
+				nudgeTime  = 2
+				circle( frame, (20, vc.height-20), 2, (0,0,255), 2 )
+			elif intersect < (vc.width/2 - 60):
+				print "BIGGER NUDGE M1!!"
+				nudgeMotor = cl.M1
+				nudgeTime  = 7
+				circle( frame, (20, vc.height-20), 6, (0,255,0), 2 )
+			elif intersect < (vc.width/2 - 30):
+				print "BIG NUDGE M1!!"
+				nudgeMotor = cl.M1
+				nudgeTime  = 4
+				circle( frame, (20, vc.height-20), 4, (0,255,0), 2 )
+			elif intersect < (vc.width/2 - 15):
+				print "NUDGE M1!!"
+				nudgeMotor = cl.M1
+				nudgeTime  = 2
+				circle( frame, (20, vc.height-20), 2, (0,255,0), 2 )
+		if nudgeMotor and nudgeTime:
 			cl.flush()
-			cl.nudge( cl.M2 )
-			test = cl.readAndCheck()
-			if not test:
-				print "nudge not received D="
-			else:
-				print "nudge received!"
-				lastNudge = vc.frameCount
-		elif intersect and intersect < (vc.width/2 - 20) and (vc.frameCount >= lastNudge+10):
-			print "NUDGE M1!!"
-			cl.nudge( cl.M1 )
+			cl.nudge( nudgeMotor, nudgeTime )
 			test = cl.readAndCheck()
 			if not test:
 				print "nudge not received D="
@@ -130,6 +161,6 @@ def testCapture():
 
 if __name__ == '__main__':
 	cl.setup()
-	testCapture()
+	# testCapture()
 	# simpleNudgeTest()
-	# mainLoop()
+	mainLoop()
