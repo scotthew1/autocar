c987eaa1821c113eb9d2dcceb09821dfab328e7f
Scott Reisor <scotthew1@mac.com>
2014-05-06 21:30:40 -0400

    added software setup doc, cleaned up setup.sh

--- setup.sh
+++ setup.sh
@@ -3,9 +3,6 @@
 # some setup stuff that should run each time
 # needs sudo to run
 
-# detect the webcam
-# modprobe uvcvideo
-
 # set properties for the webcam
 v4l2-ctl -d /dev/video0 -c exposure_auto_priority=1
 v4l2-ctl -d /dev/video0 -c exposure_auto=1
@@ -15,8 +12,3 @@ v4l2-ctl --list-ctrls
 
 # get internet working
 route add default gw 192.168.7.1
-
-# i was annoyed with the time not being current, and couldnt get ntp working
-# this is hacky and gets the time through ssh from my personal server
-# without the proper config files, this won't work on a clean install
-date --set="`ssh bender -o ConnectTimeout=3 -F /home/ubuntu/.ssh/config -i /home/ubuntu/.ssh/id_rsa date`"
